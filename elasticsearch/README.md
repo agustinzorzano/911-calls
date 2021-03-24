9651ab9f14c9c7814dd69a49d16797998757b1d4
agustin <aguszorza@gmail.com>
2021-03-24 13:40:20 +0100

    elasticSearch requests

--- elasticsearch/README.md
+++ elasticsearch/README.md
@@ -22,7 +22,95 @@ GET <nom de votre index>/_count
 À vous de jouer ! Écrivez les requêtes ElasticSearch permettant de résoudre les problèmes posés.
 
 ```
-TODO : ajouter les requêtes ElasticSearch ici
+# Compter le nombre d'appels par catégorie
+
+GET 911-calls/_search?size=0
+{
+  "aggregations": {
+    "categories": {
+      "terms": { "field": "category" }
+    }
+  }
+}
+
+# Trouver les 3 mois ayant comptabilisés le plus d'appels
+
+GET 911-calls/_search
+{
+  "size": 0,
+  "aggs": {
+    "group_by_month": {
+      "date_histogram": {
+        "field": "timestamp",
+        "interval": "month"
+      },
+      "aggs":{
+        "total_categories": {
+            "sum": {
+              "field": "e"
+            }
+          },
+        "categories_bucket_sort": {
+          "bucket_sort": {
+            "sort": [{"total_categories": {"order": "desc"}}],
+            "size": 3
+          }
+        }
+      }
+    }
+  }
+}
+
+# Trouver le top 3 des villes avec le plus d'appels pour overdose
+
+GET 911-calls/_search?size=0
+{
+  "query": {
+    "match": {
+            "title": "overdose"
+      }
+  },
+  "aggs": {
+    "categories": {
+      "terms": { "field": "neighborhood" },
+      "aggs":{
+        "total_categories": {
+          "sum": {
+            "field": "e"
+          }
+        },
+        "categories_bucket_sort": {
+          "bucket_sort": {
+            "sort": [{"total_categories": {"order": "desc"}}],
+            "size": 3
+          }
+        }
+      }
+    }
+  }
+}
+
+# Compter le nombre d'appels autour de Lansdale dans un rayon de 500 mètres
+
+GET 911-calls/_search?size=0
+{
+  "query": {
+    "bool": {
+      "must": {
+        "match_all": {}
+      },
+      "filter": {
+        "geo_distance": {
+          "distance": "500m",
+          "location": {
+            "lat": 40.241493,
+            "lon":  -75.283783
+          }
+        }
+      }
+    }
+  }
+}
 ```
 
 ## Kibana
--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -18,22 +18,65 @@ async function run() {
   await client.indices.create({
     index: INDEX_NAME,
     body : {
+      "mappings": {
+        "properties": {
+          "latitude": { "type": "double" },
+          "longitude": { "type": "double" },
+          "title": { "type": "text", "search_analyzer": "standard" },
+          "category": { "type": "keyword" },
+          "description": { "type": "text" },
+          "zip": { "type": "integer" },
+          "timestamp": { "type": "date", "format": "yyyy-MM-dd HH:mm:ss" },
+          "neighborhood": { "type": "keyword" },
+          "address": { "type": "text" },
+          "e": { "type": "integer" },
+          "location": { "type": "geo_point" },
+        }
+      }
       // TODO configurer l'index https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
     }
   });
 
+  let calls = [];
+
   fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      const call = { 
+      const call = {
+        "latitude": data.lat,
+        "longitude": data.lng,
+        "category": data.title.split(':')[0],
+        "description": data.desc,
+        "zip": data.zip,
+        "title": data.title,
+        "timestamp": data.timeStamp,
+        "neighborhood": data.twp,
+        "address": data.addr,
+        "e": data.e,
+        "location" : [parseFloat(data.lng), parseFloat(data.lat)]
       };
+      calls.push(call);
       // TODO créer l'objet call à partir de la ligne
     })
     .on('end', async () => {
+      const query = createBulkInsertQuery(calls, INDEX_NAME)
+      client.bulk(query, (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`Inserted ${resp.body.items.length} calls`);
+        client.close();
+      });
       // TODO insérer les données dans ES en utilisant l'API de bulk https://www.elastic.co/guide/en/elasticsearch/reference/7.x/docs-bulk.html
     });
-  
+}
 
+function createBulkInsertQuery(calls, index_name) {
+  const body = calls.reduce((acc, call) => {
+    acc.push({ index: { _index: index_name, _type: '_doc' } })
+    acc.push({ ...call })
+    return acc
+  }, []);
+
+  return { body };
 }
 
 run().catch(console.log);
